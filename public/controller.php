0d2718e9bd90097faf83f20701e44cd22b599755
Ben <ben@Ben-taylor.co.uk>
2020-01-03 20:38:37 +0000

    add config.php update controller.php to die on unaccepted request method load variables from url string load controller file if it exists return result if it is present

--- public/controller.php
+++ public/controller.php
@@ -1,3 +1,28 @@
-<html>
-    hello werld
-</html>
+<?php
+
+include('../config/config.php');
+
+$allowed_methods = [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
+];
+
+// block requests not in the allowed_methods
+if (!in_array($_SERVER['REQUEST_METHOD'], $allowed_methods)) {
+    die;
+}
+
+// get our vars from the url
+$vars = explode('/', $_SERVER['REQUEST_URI']);
+$params = array_slice($vars, 2);
+
+// check the controller exists and load it
+if (file_exists(DIR_ROOT . $vars[1])) {
+    include_once(DIR_ROOT . $vars[1]);
+}
+
+if (isset($return)) {
+    echo json_encode($return);
+}
