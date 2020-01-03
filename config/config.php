6caa376b7f60cac1db1814bd5785631bba359839
Ben <ben@Ben-taylor.co.uk>
2020-01-03 20:49:36 +0000

    add extra paths to config load in correct path to the controller add demo members controller

--- config/config.php
+++ config/config.php
@@ -1,3 +1,7 @@
 <?php
 
 define('DIR_ROOT', '../');
+define('DIR_CLASS', DIR_ROOT . 'classes/');
+define('DIR_CONFIG', DIR_ROOT . 'config/');
+define('DIR_CONTROL', DIR_ROOT . 'controllers/');
+define('DIR_MODEL', DIR_ROOT . 'models/');
--- public/controller.php
+++ public/controller.php
@@ -19,8 +19,8 @@ $vars = explode('/', $_SERVER['REQUEST_URI']);
 $params = array_slice($vars, 2);
 
 // check the controller exists and load it
-if (file_exists(DIR_ROOT . $vars[1])) {
-    include_once(DIR_ROOT . $vars[1]);
+if (file_exists(DIR_CONTROL . $vars[1] . '.php')) {
+    include_once(DIR_CONTROL . $vars[1] . '.php');
 }
 
 if (isset($return)) {
