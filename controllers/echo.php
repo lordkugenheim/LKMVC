73bdd73fd2427a6874e8a9bdf58665081234e221
Ben <ben@Ben-taylor.co.uk>
2020-01-10 22:12:25 +0000

    add check for a malformed request remove duplicated code

--- controllers/echo.php
+++ controllers/echo.php
@@ -1,16 +1,21 @@
 <?php
 
+//TODO build a set of re-usable responses?
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
-        $output = trim($_POST['message']);
-        Core\Controller::getinstance()->addOutput(['message' => $output]);
-        Core\Controller::getinstance()->setHttpCode(200);
+        if (isset($_POST['message'])) {
+            $output = trim($_POST['message']);
+            Core\Controller::getinstance()->setHttpCode(200);
+        } else {
+            $output = 'Malformed request';
+        }
         break;
     default:
         $header = 'Allow: POST';
         $output = 'Invalid request method. Allowed methods are POST';
         Core\Controller::getinstance()->addHeader($header);
-        Core\Controller::getinstance()->addOutput(['message' => $output]);
         Core\Controller::getinstance()->setHttpCode(405);
         break;
 }
+
+Core\Controller::getinstance()->addOutput(['message' => $output]);
