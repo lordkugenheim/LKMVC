ddfe36c19472e6b8209fbb0a168382d29f1e1e1f
Ben <ben@Ben-taylor.co.uk>
2020-01-12 18:26:14 +0000

    add prototype initialiser for endpoints

--- public/index.php
+++ public/index.php
@@ -6,7 +6,14 @@ require __DIR__ . '/../config/config.php';
 require DIR_ROOT . 'vendor\autoload.php';
 require DIR_CONTROL . 'controller.php';
 
-$endpoint = explode('/', $_SERVER['REQUEST_URI'])[1];
+$endpoint_name = explode('/', $_SERVER['REQUEST_URI'])[1];
+
+if (Controller::endpointExists($endpoint)) {
+    $endpoint = Controller::getEndpoint($endpoint);
+    if ($endpoint->isvalidRoute()) {
+        $endpoint->go();
+    }
+}
 
 //TODO: investigate autoloading to replace this
 // check the model exists and load it
@@ -16,11 +23,6 @@ if (file_exists(DIR_MODEL . $endpoint . '.php')) {
 // check the controller exists and load it
 if (file_exists(DIR_CONTROL . $endpoint . '.php')) {
     include_once(DIR_CONTROL . $endpoint . '.php');
-}
-
-$classname = 'Core\\' . ucwords($endpoint);
-if (class_exists($classname)) {
-    $class = new $classname();
 }
 
 Controller::getinstance()->send();
