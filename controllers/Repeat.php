2412c1cd9ead1fe4ff3f387cae2a227a6be165be
Ben <ben@ben-taylor.co.uk>
2020-10-02 13:50:53 +0100

    add repeat and repeatmodel

--- controllers/Repeat.php
+++ controllers/Repeat.php
@@ -2,13 +2,10 @@
 
 class Repeat extends Controller
 {
-    public function __construct()
-    {
-        parent::__construct('Repeat');
-    }
-
     public function httpGet()
     {
-        
+        $request_data = $this->request->other_parameters;
+        $model_data = $this->model->httpGet($request_data);
+        var_dump($model_data);
     }
 }
--- models/RepeatModel.php
+++ models/RepeatModel.php
@@ -2,8 +2,20 @@
 
 class RepeatModel
 {
-    public function httpGet()
+    public function httpGet($data)
     {
-        
+        if (!empty($data)) {
+            return [
+                'data' => $data[0],
+                'status' => 'success',
+                'http_status' => 200,
+            ];
+        } else {
+            return [
+                'data' => 'no repeat value sent',
+                'status' => 'error',
+                'http_status' => 400,
+            ];
+        }
     }
 }
