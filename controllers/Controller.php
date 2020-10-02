4b32ddd438e2dca678ef62bfb63d577535554f9d
Ben <ben@ben-taylor.co.uk>
2020-10-02 13:21:45 +0100

    add repeatmodel add base controller methods and properties

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -2,5 +2,25 @@
 
 class Controller
 {
+    protected $request;
+    protected $view;
+    protected $model;
+
+    public function __construct()
+    {
+        $this->request = new Request();
+        $this->loadModel(get_called_class());
+    }
+
+    public function loadView($view_name)
+    {
+        $this->view = new $view_name();
+    }
+    
+    public function loadModel($model_name)
+    {
+        $model_name .= 'Model';
+        $this->model = new $model_name();
+    }
     
 }
