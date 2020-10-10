f697fc5c0a7af93a9d191ce4f530e4adcd0ec180
Ben <ben@ben-taylor.co.uk>
2020-10-10 20:40:17 +0100

    build database class

--- app/Database.php
+++ app/Database.php
@@ -6,7 +6,7 @@ class Database
 
     public function __construct()
     {
-        $this->dbh = new PDO('mysql:host=' . MYSQL_HOST . 'dbname=' . MYSQL_DBNAME, MYSQL_USER, MYSQL_PASS);
+        $this->dbh = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DBNAME, MYSQL_USER, MYSQL_PASS);
     }
 
     public function __destruct()
@@ -14,7 +14,36 @@ class Database
         $this->dbh = null;
     }
 
-    public function query($sql, $args = [])
+    public function select($sql, $args = [])
+    {
+        $result = $this->query($sql, $args);
+        return $result ? $result : [];
+    }
+
+    public function insert($sql, $args = [])
+    {
+        return $this->transaction($sql, $args);
+        // returns id of insert(s)
+    }
+
+    public function update($sql, $args = [])
+    {
+        // returns rows affected
+    }
+
+    private function query($sql, $args = [])
+    {
+        try {
+            $stmt = $this->dbh->prepare($sql);
+            $stmt->execute($args);
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            return $e;
+            //do something with $e
+        }
+    }
+
+    private function transaction($sql, $args = [])
     {
         try {
             $this->dbh->beginTransaction();
@@ -22,7 +51,7 @@ class Database
             foreach ($args as $key => $value) {
                 $this->dbh->bindParam($key, $value);
             }
-            $this->dhb->commit();
+            $this->dbh->commit();
         } catch (Exception $e) {
             $this->dbh->rollback();
             // do something with $e
