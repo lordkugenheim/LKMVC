fb7d5ecdfcb97d187ab7b46abaf746ba636b436e
Ben <ben@ben-taylor.co.uk>
2020-10-14 17:49:13 +0100

    Add comments

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -1,11 +1,13 @@
 <?php
 
+// load any php file inside the /config folder
 foreach (scandir('../config') as $filename) {
     if (strpos($filename, 'php') !== false) {
         require_once '../config/' . $filename;
     }
 }
 
+// Autoloader for core App, Controller and Model classes
 spl_autoload_register(function ($class) {
     $class = str_replace('\\', '/', $class);
     foreach ([DIR_APP, DIR_CONTROL, DIR_MODEL] as $folder) {
--- app/Core.php
+++ app/Core.php
@@ -1,9 +1,23 @@
 <?php
 
+/**
+ * Core class for LKMVC Framework
+ *
+ * This class begins the application by determining the
+ * requested endpoint and loading the associated controller
+ *
+ * @author Ben Taylor-Wilson <ben@ben-taylor.co.uk>
+ * @see http://www.ben-taylor.co.uk/LKMVC
+ */
 class Core
 {
     private $controller;
 
+    /**
+     * Determines controller and method
+     * to load and then calls them
+     * @return bool
+     */
     public function initialise()
     {
         if ($this->callController(Request::controller())) {
@@ -15,6 +29,11 @@ class Core
         return false;
     }
 
+    /**
+     * Check a particular controller's class' file
+     * exists and instantiate it into $this->controller
+     * @param $controller string name of controller to instantiate
+     */
     private function callController($controller)
     {
         if (file_exists(DIR_CONTROL . $controller . '.php')) {
@@ -24,6 +43,11 @@ class Core
         return false;
     }
 
+    /**
+     * Check a particular method exists in the
+     * loaded controller and then call it
+     * @param $method string name of the method to access
+     */
     private function callMethod($method)
     {
         if (get_class($this->controller) && method_exists($this->controller, $method)) {
